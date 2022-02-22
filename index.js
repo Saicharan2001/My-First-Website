1d33bec590ca8add84b9c8b0d4c888931ba5fcec
K.SAICHARAN <saicharan2001kande@gmail.com>
2022-02-22 18:05:25 +0530

    New login

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const https = require("https");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
-const Port = 80;
+const Port = 3000;
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -17,6 +17,6 @@ app.post("/", function (req, res) {
   res.write("<p>You can peacefully move back to the home page</p>");
   res.send();
 });
-app.listen(process.env.PORT || 80, function (req, res) {
+app.listen(process.env.PORT || 3000, function (req, res) {
   console.log("Server is started");
 });
